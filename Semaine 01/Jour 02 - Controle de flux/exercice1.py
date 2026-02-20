91d6907ed21e724eb6f3253372c4c780b5f34907
aubin-pierre7 <aubinp58@gmail.com>
2026-02-20 21:13:16 +0100

    Fin de lexercice 1

---
+++ Semaine 01/Jour 02 - Controle de flux/exercice1.py
@@ -0,0 +1,12 @@
+#Verifier si un nombre est premier ou pas
+nombre = int(input("Entrez un nombre: "))
+
+if nombre > 1:
+    for i in range(2, int(nombre**0.5) + 1):
+        if nombre % i == 0:
+            print(f"{nombre} n'est pas un nombre premier.")
+            break
+    else:
+        print(f"{nombre} est un nombre premier.")
+else:
+    print(f"{nombre} n'est pas un nombre premier.")        
