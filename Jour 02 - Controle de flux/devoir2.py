2c7ba59aa5581e7289fe3324353444cc324af7c3
aubin-pierre7 <aubinp58@gmail.com>
2026-02-21 14:07:06 +0100

    Resolution du devoir 2

--- Jour 02 - Controle de flux/devoir2.py
+++ Jour 02 - Controle de flux/devoir2.py
@@ -1,1 +1,11 @@
 #Programme qui trouve le plus grand nombre dans une liste en utilisant une boucle for
+
+liste = [5, 12, 7, 3, 9]
+
+maxi = liste[0]          # On initialise le maximum avec le premier élément
+
+for nombre in liste:      # Parcours de tous les éléments
+    if nombre > maxi:     # Si on trouve un nombre plus grand
+        maxi = nombre    # On met à jour le maximum
+
+print(f"Le plus grand nombre de la liste est {maxi}")
