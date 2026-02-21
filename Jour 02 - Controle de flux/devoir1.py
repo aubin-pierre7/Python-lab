c9b2bcc945b51e541bede332a1db7603b32574ff
aubin-pierre7 <aubinp58@gmail.com>
2026-02-21 13:50:24 +0100

    Realisation du premier devoir

--- Jour 02 - Controle de flux/devoir1.py
+++ Jour 02 - Controle de flux/devoir1.py
@@ -1,1 +1,9 @@
 #Programme qui calcule le factoriel d'un nombre en utilisant une boucle while
+
+nombre = int(input("Entrez le nombre dont vous voulez le factoriel: "))
+resultat = 1
+i=1
+while i <= nombre:
+    resultat *= i
+    i += 1
+print(f"le factoriel de {nombre} est {resultat}.")    
