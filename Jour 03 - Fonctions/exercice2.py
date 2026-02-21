08d63b0d8f3d66157702940e6e0d3baed546d3a8
aubin-pierre7 <aubinp58@gmail.com>
2026-02-21 23:54:07 +0100

    Fichier pour exercice2

---
+++ Jour 03 - Fonctions/exercice2.py
@@ -0,0 +1,15 @@
+#Creer un module pour les fonctions mathematiques de base
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erreur: Division par zéro"
